46e0da48d165f83ddd14b3c29bdff5bc46fb829c
“heyhodz <duzhen2018@163.com>
2018-12-23 22:16:45 +0800

    注册第三次提交

--- sqxs/src/action/user.js
+++ sqxs/src/action/user.js
@@ -5,6 +5,20 @@ export const get_user_action=(username)=>({
         fetch(url)
         .then((res)=>res.json())
         .then((data)=>{
+            resolve(data)
+        })
+    })
+})
+export const set_user_action=(username,userpwd)=>({
+    type:"SET_USER_MESSAGE",
+    payload:new Promise(resolve=>{
+        let url="http://localhost:3000/submit"
+        fetch(url,{
+            uname:username,
+            upwd:userpwd
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
             console.log(data)
         })
     })
--- sqxs/src/store/index.js
+++ sqxs/src/store/index.js
@@ -4,8 +4,9 @@ import reduxPromiseMiddleware from "redux-promise-middleware";
 import classify from './reducers/classify'
 import topReducers from './reducers/topReducers'
 import bookList from './reducers/bookList'
+import user from "./reducers/user"
 const reducers = combineReducers({
-    classify,topReducers,bookList
+    classify,topReducers,bookList,user
 })
 
 const store = createStore(reducers,applyMiddleware(reduxPromiseMiddleware()));
