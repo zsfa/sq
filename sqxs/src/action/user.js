5b99b2f1de49f51989672df069eb67394bb5104a
“heyhodz <duzhen2018@163.com>
2018-12-24 10:52:31 +0800

    注册完成

--- sqxs/src/action/user.js
+++ sqxs/src/action/user.js
@@ -1,3 +1,5 @@
+import {fetch} from "whatwg-fetch"
+import axios from "axios"
 export const get_user_action=(username)=>({
     type:"GET_USER_MESSAGE",
     payload:new Promise(resolve=>{
@@ -13,13 +15,22 @@ export const set_user_action=(username,userpwd)=>({
     type:"SET_USER_MESSAGE",
     payload:new Promise(resolve=>{
         let url="http://localhost:3000/submit"
-        fetch(url,{
+        axios.post(url,{
             uname:username,
             upwd:userpwd
-        })
-        .then((res)=>res.json())
+        }).then(res=>res.json)
         .then((data)=>{
             console.log(data)
         })
+
+
+        // fetch(url,{
+        //     uname:username,
+        //     upwd:userpwd
+        // })
+        // .then((res)=>res.json())
+        // .then((data)=>{
+        //     console.log(data)
+        // })
     })
 })
